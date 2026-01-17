9ffaaf5ba7e70d94cf48d24d16694afe9e6ddd6e
Ghosuto <clash.raja10@gmail.com>
2026-01-17 20:06:52 +0530

    dolby: Import LunarisDolby app
    * Imported from https://github.com/Pong-Development/hardware_dolby
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: kenway214 <kenway214@outlook.com>
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- dolby.mk
+++ dolby.mk
@@ -103,6 +103,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     DSPVolumeSynchronizer
 
+# LunarisDolby
+PRODUCT_PACKAGES += \
+    LunarisDolby
+
 # Dolby Proprietary blobs
 PRODUCT_COPY_FILES += \
     $(DOLBY_PATH)/proprietary/vendor/etc/init/vendor.dolby.hardware.dms@2.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.dolby.hardware.dms@2.0-service.rc \
